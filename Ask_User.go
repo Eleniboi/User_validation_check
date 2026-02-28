724f45c3d1ce0d908a71f9894362707143db474d
somafu <omafuoloche4@gmail.com>
2026-02-28 16:15:44 +0000

    user validation

---
+++ Ask_User.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+/*this program will ask user for a
+number and print all numbers from 1 up to that users
+number, if the user enters a non numeric character or
+a 0 value print error and stop! */
+
+func main() {
+
+	var i int
+	fmt.Print("Enter a number: ")
+
+	_, err := fmt.Scanln(&i)
+
+	if err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+	if i < 1 {
+		fmt.Println("Invalid Number Enter a Valid Number: ")
+		return
+	}
+	for p := 1; p <= i; p++ {
+		if p%2 == 0 {
+			fmt.Println(p)
+		}
+	}
+
+}
